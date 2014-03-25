85d3dd62e467acf8347c701ad5f9ac2525c4b503
Duncan Graham Walker <walkerdu@bc.edu>
2014-03-24 22:10:44 -0400

    Readme and src

--- README.md
+++ README.md
@@ -1,4 +1,90 @@
-selaction
+Selaction
 =========
 
-A &lt;select> component for ember that allows actions and link-to transitions to be attached to options
+A &lt;select> component for Ember.js that allows actions and link-to transitions to be attached to &lt;options>
+
+Installation
+------
+
+Simply copy and paste selaction.js into the components directory of your ember app
+
+Usage
+------
+
+**Template**
+
+```handlebars
+{{#sa-select}}
+  {{#sa-placeholder}}Choose...{{/sa-placeholder}}
+  {{#sa-option action='copy' target='view'}}Copy{{/sa-option}}
+  {{#sa-option link-to='campaign.content'}}Compare{{/sa-option}}
+{{/sa-select}}
+```
+
+**Action on route or controller**
+
+Selaction allows you to attach an action to each option.
+
+```handlebars
+{{#sa-option action='copy'}}Copy{{/sa-option}}
+```
+
+```javascript
+App.YourController = Em.ObjectController.extend({
+  actions: {
+    copy: function() {
+      // Action logic here
+    }
+  }
+});
+```
+
+**Action on view**
+
+Selaction allows you to attach an action to each option.
+
+```handlebars
+{{#sa-option action='copy' target='view'}}Copy{{/sa-option}}
+```
+
+```javascript
+App.YourView = Em.View.extend({
+  actions: {
+    copy: function() {
+      // Action logic here
+    }
+  }
+});
+```
+
+**Link to Another Route**
+
+Selaction also allows you to attach a route transition to an option (like a ```{{#link-to}}``` helper would).
+
+```handlebars
+  {{#sa-option link-to='about'}}Compare{{/sa-option}}
+```
+
+```javascript
+App.Router.map(function() {
+  this.route('about', { path: '/about' });
+});
+```
+
+General Options
+------
+
+**Class Prefix** sets the prefix used for css targeting of each element
+
+```handlebars
+{{#sa-select prefix='eg'}}
+  {{!-- Option components here...--}}
+{{/sa-select}}
+```
+
+If you need more advanced customizations you can edit the source.
+
+Contributing & Bugs
+------
+
+Issue a PR or give me a holla.
---
+++ selaction.js
@@ -0,0 +1,109 @@
+/* Register namespace */
+
+Selaction = Em.Namespace.create({
+  VERSION: '0.1.0',
+});
+
+/* Generic Classes */
+
+Selaction.Core = Em.Mixin.create({
+
+  selectComponent: function() {
+    var className = Selaction.SelectWrapperComponent
+    return this.nearestOfType(className);
+  }.property(),
+
+  _class: function(modifier) {
+    var prefix = this.get('selectComponent.prefix') || this.get('prefix');
+    return prefix + '-' + modifier;
+  },
+
+});
+
+/* Main component on which options are set in the template */
+
+Selaction.SelectWrapperComponent = Em.Component.extend(
+  Selaction.Core,
+  Em.ViewTargetActionSupport, {
+
+  classNames: ['ember-select'],
+  classNameBindings: ['selectClass'],
+  tagName: 'select',
+  prefix: 'sa', // Overwrite on component in template
+
+  change: function(event) {
+    var selected = event.target.selectedOptions[0];
+    var action = $(selected).data('action');
+    var target = $(selected).data('target'); /* Optional with action */
+    var linkTo = $(selected).data('link-to');
+    var view = this.get('parentView');
+
+    if (action) {
+      this._action(action, target, view);
+    } else if (linkTo) {
+      this._linkTo(linkTo, view);
+    } else {
+      Em.assert('You did not specify an action or link-to for the chosen menu-item component in your template');
+    }
+  },
+
+  selectClass: function() {
+    return this._class('select');
+  }.property('selectComponent.prefix'),
+
+  _action: function(action, target, view) {
+
+    if (target == 'view') {
+      target = view;
+    }
+
+    this.triggerAction({
+      action: action,
+      target: target,
+    });
+  },
+
+  _linkTo: function(linkTo, view) {
+    view.get('controller').transitionToRoute(linkTo);
+  },
+});
+
+Em.Handlebars.helper('sa-select', Selaction.SelectWrapperComponent);
+
+/* Individual option component - has an action (with optional target) or a link-to */
+
+Selaction.MenuOptionComponent = Em.Component.extend(
+  Selaction.Core, {
+
+  attributeBindings: [
+    'action:data-action',
+    'target:data-target',
+    'link-to:data-link-to',
+  ],
+
+  classNames: ['ember-option'],
+  classNameBindings: ['optionClass'],
+  tagName: 'option',
+  role: 'menuitem',
+
+  optionClass: function() {
+    return this._class('option');
+  }.property('selectComponent.prefix'),
+});
+
+Em.Handlebars.helper('sa-option', Selaction.MenuOptionComponent);
+
+/* Placeholder component (default option for select) */
+
+Selaction.MenuPlaceholderComponent = Selaction.MenuOptionComponent.extend({
+  attributeBindings: ['disabled', 'selected',],
+  classNameBindings: ['placeholderClass'],
+  disabled: true,
+  selected: true,
+
+  placeholderClass: function() {
+    return this._class('placeholder');
+  }.property('selectComponent.prefix'),
+});
+
+Em.Handlebars.helper('sa-placeholder', Selaction.MenuPlaceholderComponent);
